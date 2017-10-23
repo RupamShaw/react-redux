fb736aae2d3f32d1358c03c4b024d633ad2dff50
Rupam <rupam.shaw@gmail.com>
2017-10-23 15:05:11 +1100

    redux counter all in index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { createStore }  from 'redux'
-
 import registerServiceWorker from './registerServiceWorker';
 
 const Counter =({
