bb205d8a903a13c9bd2ef5c807cb20f19d280821
Rupam <rupam.shaw@gmail.com>
2017-10-26 02:10:59 +1100

    mapstatetoprops and mapdispatchto props and bankcreators removed current_account

--- src/reducers/bank/bankReducer.js
+++ src/reducers/bank/bankReducer.js
@@ -5,8 +5,7 @@ const initialState = {
 const bankReducer = (state = initialState.balance, action) => {
     console.log(action); //Temporarily logging all actions
     switch (action.type) {
-        case constants.CREATE_ACCOUNT:
-            return initialState;
+
         case constants.DEPOSIT_INTO_ACCOUNT:
             return { balance: state.balance + parseFloat(action.amount) };
         case constants.WITHDRAW_FROM_ACCOUNT:
